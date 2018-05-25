12dfa9e3f8319f27dd8627269cab72b9e5c275e3
Orozco Hsu <orozcohsu@hotmail.com>
2018-05-25 08:34:23 +0300

    update tcfst.p

--- docker/tcfst.py
+++ docker/tcfst.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
 Created on Thu May 17 16:27:21 2018
 
@@ -9,6 +8,7 @@ This is a demo cralwering to S3 crawler-20180517
 
 from boto3.s3.transfer import S3Transfer
 import boto3
+import io
 
 AWS_ACCESS_KEY_ID = 'AKIAIL4R7JYZ3JJTSTFQ'
 AWS_SECRET_ACCESS_KEY = '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'
@@ -28,13 +28,12 @@ def get_head_text(url, head_tag):
 import json
 url = 'http://blog.castman.net/web-crawler-tutorial/ch1/connect.html'
 title = get_head_text(url, 'h1')
-print(json.dumps(title))
-print(json.dumps(title,ensure_ascii=False))
 
 j = {'title': title, 'url': url}
+s=json.dumps(j,ensure_ascii=False,encoding='utf8')
 
-with open("output.json","w") as output:
-    output.write(json.dumps(j,ensure_ascii=False))
+with io.open('output.json','w',encoding='utf8') as file:
+    file.write(s)
 
 
 client = boto3.client('s3', aws_access_key_id=AWS_ACCESS_KEY_ID,aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
